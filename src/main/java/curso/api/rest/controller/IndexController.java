d9f5d892f1fb42607a7ad9c1d3729ad949701a52
Paulo Gomes <gomes7.paulo@gmail.com>
2021-06-20 14:14:54 -0300

    first commit

---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,255 @@
+package curso.api.rest.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.TelefoneRepository;
+import curso.api.rest.repository.UsuarioRepository;
+import curso.api.rest.service.ImplementacaoUserDetailsService;
+
+
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value = "/usuario")
+public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
+	
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+	
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+		
+	}
+	
+	@GetMapping(value = "v1/{id}", produces = "application/json")
+	@Cacheable("cacheuser")
+	public ResponseEntity<Usuario> buscarPorIdV1(@PathVariable(value = "id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
+		System.out.println("Buscar por id V1");
+		return ResponseEntity.ok(usuario);
+	}
+
+	@GetMapping(value = "v2/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> buscarPorIdV2(@PathVariable(value = "id") Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
+		System.out.println("Buscar por id V2");
+		return ResponseEntity.ok(usuario);
+	}
+	
+	
+	@GetMapping(value = "/v2", produces = "application/json", headers = "X-API-Version=v2")
+	public ResponseEntity<List<Usuario>> buscarTodosV2() {
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
+		System.out.println("Buscar Todos V2");
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}	
+	
+	
+	
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Page<Usuario>> usuario () throws InterruptedException{
+		
+		PageRequest page = PageRequest.of(0, 5, Sort.by("nome"));
+		
+		Page<Usuario> list = usuarioRepository.findAll(page);
+		
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
+		
+	}
+	
+	
+	
+	@GetMapping(value = "/page/{pagina}", produces = "application/json")
+	public ResponseEntity<Page<Usuario>> usuarioPagina (@PathVariable("pagina") int pagina) throws InterruptedException{
+		
+		PageRequest page = PageRequest.of(pagina, 5, Sort.by("nome"));
+		
+		Page<Usuario> list = usuarioRepository.findAll(page);
+		
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
+		
+	}
+	
+	
+	
+	
+	@GetMapping(value = "/{id}/codigovenda/{venda}", produces = "application/json")
+	public ResponseEntity<Usuario> relatorio(@PathVariable (value = "id") Long id
+											, @PathVariable (value = "venda") Long venda) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+	
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);		
+	}
+	
+	
+	
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		
+		// amarrar filhos aos pais: telefones ao usuario
+		for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		
+		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhacriptografada);
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		implementacaoUserDetailsService.insereAcessoPadrao(usuarioSalvo.getId());
+		
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		
+		// amarrar filhos aos pais: telefones ao usuario
+				for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+					usuario.getTelefones().get(pos).setUsuario(usuario);
+				}
+				
+		
+				
+		Usuario userTemporario = usuarioRepository.findUserByLogin(usuario.getLogin());
+		
+		if (!userTemporario.getSenha().equals(usuario.getSenha())) {
+			String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(senhacriptografada);
+		}
+		
+		
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}
+	
+	
+	/*
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		
+		// amarrar filhos aos pais: telefones ao usuario
+				for (int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+					usuario.getTelefones().get(pos).setUsuario(usuario);
+				}
+		
+		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhacriptografada);
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+		
+	}*/
+	
+	
+	
+	
+	
+	
+	@PutMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> atualizarSetId(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+		usuario.setId(id);
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable("id") Long id) {
+		usuarioRepository.deleteById(id);
+		return "ok";
+	}
+	
+	
+	/*
+	//END-POINT 
+	@GetMapping(value = "/usuarioPorNome/{nome}", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuarioPorNome (@PathVariable("nome") String nome) throws InterruptedException{
+			
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findUserByNome(nome);
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+		
+	} */
+	
+	
+	//END-POINT 
+	@GetMapping(value = "/usuarioPorNome/{nome}", produces = "application/json")
+	public ResponseEntity<Page<Usuario>> usuarioPorNome (@PathVariable("nome") String nome) throws InterruptedException{
+		
+		PageRequest pageRequest = null;
+		Page<Usuario> list = null;
+		
+		if (nome == null || (nome != null && nome.trim().isEmpty()) || nome.equalsIgnoreCase("undefined")) {
+			pageRequest = PageRequest.of(0, 5, Sort.by("nome"));
+			list = usuarioRepository.findAll(pageRequest);
+		} else {
+			pageRequest = PageRequest.of(0, 5, Sort.by("nome"));
+			list = usuarioRepository.findUserByNamePage(nome, pageRequest);
+		}
+						
+		return new ResponseEntity<Page<Usuario>>(list, HttpStatus.OK);
+		
+	}
+	
+	
+	
+	
+	
+	@Autowired
+	private TelefoneRepository telefoneRepository;
+	
+	@DeleteMapping(value = "/removerTelefone/{id}", produces = "application/text")
+	public String deleteTelefone(@PathVariable("id") Long id) {
+		
+		telefoneRepository.deleteById(id);
+		return "ok";
+		
+	}
+	
+	
+
+}
---
+++ src/main/java/curso/api/rest/security/JWTTokenAutenticacaoService.java
@@ -0,0 +1,134 @@
+package curso.api.rest.security;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import curso.api.rest.ApplicationContextLoad;
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+@Component
+public class JWTTokenAutenticacaoService {
+
+	// Tempo de validade do TOKEN - 2 dias
+	private static final long EXPIRATION_TIME = 172800000;
+	
+	// Senha única para compor a autenticação e ajudar na segurança
+	private static final String SECRET = "SenhaExtremamenteSecreta";
+	
+	// Prefixo padrão de Token
+	private static final String TOKEN_PREFIX = "Bearer";
+	
+	private static final String HEADER_STRING = "Authorization";
+	
+	// Gerando token de autenticação e adicionando ao cabeçalho e resposta Http
+	public void addAuthentication(HttpServletResponse response, String username) throws IOException {
+		
+		// Montagem do Token
+		String JWT = Jwts.builder() // gerador de token
+				.setSubject(username) // adiciona usuário
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // tempo expiração
+				.signWith(SignatureAlgorithm.HS512, SECRET) // algoritmo de geração de senha
+				.compact(); // compactação String
+		
+		String token = TOKEN_PREFIX + " " + JWT; // Bearer 3498hih345jkh345ui53iu5hyi
+		
+		// adiciona token no cabeçalho http
+		response.addHeader(HEADER_STRING, token); // Authorization: Bearer 3498hih345jkh345ui53iu5hyi
+		
+			
+		//atualiza token
+		ApplicationContextLoad
+		.getApplicationContext()
+		.getBean(UsuarioRepository.class).atualizaTokenUser(JWT, username);
+		
+		
+		
+		liberarCORS(response);
+		
+		// adiciona token como resposta no corpo do http
+		response.getWriter().write("{\"Authorization\": \"" + token + "\"}");
+	}
+	
+	// Retorna o usuário validado com token ou caso não seja válido retorna null
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		
+		// Pega o token enviado no cabeçalho Http
+		String token = request.getHeader(HEADER_STRING);
+		
+		
+		try {
+		if (token != null) {		
+			
+			String tokenLimpo = token.replace(TOKEN_PREFIX, "").trim();
+			
+			// Faz validação do Token do usuário na requisição
+			String user = Jwts.parser()
+					.setSigningKey(SECRET)
+					.parseClaimsJws(tokenLimpo)
+					.getBody()
+					.getSubject();	
+			
+			if (user != null) {		
+				
+				Usuario usuario = ApplicationContextLoad
+						.getApplicationContext()
+						.getBean(UsuarioRepository.class)
+						.findUserByLogin(user);
+				
+				if (usuario != null) {
+					
+					if (tokenLimpo.equalsIgnoreCase(usuario.getToken())) {
+					
+					return new UsernamePasswordAuthenticationToken(
+							usuario.getLogin(), 
+							usuario.getSenha(), 
+							usuario.getAuthorities());
+					}
+				}
+			}
+		} /*fim condicao*/
+		} catch (io.jsonwebtoken.ExpiredJwtException e) {
+			try {
+				response.getOutputStream().println("seu token expirou, faça o login ou informe novo token");
+			} catch (IOException e1) {
+								
+			}
+		}
+		
+		liberarCORS(response);
+		return null;
+	}
+
+	// CORS policy
+	private void liberarCORS(HttpServletResponse response) {
+		if (response.getHeader("Access-Control-Allow-Origin") == null) {
+			response.addHeader("Access-Control-Allow-Origin", "*");		
+		}
+		
+		if (response.getHeader("Access-Control-Allow-Headers") == null) {
+			response.addHeader("Access-Control-Allow-Headers", "*");		
+		}
+		
+		if (response.getHeader("Access-Control-Request-Headers") == null) {
+			response.addHeader("Access-Control-Request-Headers", "*");		
+		}
+		
+		if (response.getHeader("Access-Control-Allow-Methods") == null) {
+			response.addHeader("Access-Control-Allow-Methods", "*");		
+		}
+
+	}
+
+}
